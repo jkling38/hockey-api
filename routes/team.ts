815fec8c8aeac29b06ef826b521ae66c7f69e7cd
Jared Kling <jkling38@gmail.com>
2020-10-11 19:40:00 -0500

    Add some endpoints for interacting with teams

--- routes/team.ts
+++ routes/team.ts
@@ -1,37 +1,72 @@
 import { Router, Request, Response } from "express";
-import LeagueService from "../services/leagueService";
+import {
+  getTeams,
+  createTeam,
+  getTeam,
+  updateTeam,
+} from "../services/teamService";
+import { celebrate, Joi, Segments } from "celebrate";
+import { CreateTeamRequest } from "../data/request/createTeamRequest";
+import { UpdateTeamRequest } from "../data/request/updateTeamRequest";
 
 const router = Router();
 
-router.get("/:leagueId", async function (req: Request, res: Response) {
-  const { leagueId } = req.params;
-
-  if (!Number(leagueId)) {
-    res.sendStatus(400).end();
-  }
-
-  const league = await LeagueService.getLeagueById(Number(leagueId));
-
-  if (!league) {
-    res.sendStatus(404).end();
-  }
-
-  res.json(league);
-});
-
-router.get("/:leagueId/:teamId", async function (req: Request, res: Response) {
-  const { leagueId, teamId } = req.params;
-
-  const team = await LeagueService.getTeamById(
-    Number(leagueId),
-    Number(teamId)
+router
+  .route("/")
+  .get(async (_req: Request, res: Response) => {
+    console.log("Getting teams");
+    const teams = await getTeams();
+    res.send(teams);
+  })
+  .post(
+    celebrate({
+      [Segments.BODY]: {
+        name: Joi.string().required(),
+        location: Joi.string().required(),
+        abbreviation: Joi.string().required(),
+        shortName: Joi.string().required(),
+        firstYear: Joi.number(),
+        franchiseId: Joi.number(),
+      },
+    }),
+    async (req: Request, res: Response) => {
+      const created = await createTeam(req.body as CreateTeamRequest);
+      res.send(created);
+    }
   );
 
-  if (!team) {
-    res.sendStatus(404).end();
-  }
+router
+  .route("/:id")
+  .all(
+    celebrate({
+      [Segments.PARAMS]: {
+        id: Joi.number().required(),
+      },
+    })
+  )
+  .get(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const team = await getTeam(Number(id));
+    res.send(team);
+  })
+  .patch(
+    celebrate({
+      [Segments.BODY]: {
+        name: Joi.string(),
+        location: Joi.string(),
+        active: Joi.bool(),
+        abbreviation: Joi.string().required(),
+        shortName: Joi.string().required(),
+      },
+    }),
+    async (req: Request, res: Response) => {
+      const updateRequest = req.body as UpdateTeamRequest;
+      const { id } = req.params;
 
-  res.json(team);
-});
+      const updated = await updateTeam(Number(id), updateRequest);
+
+      res.send(updated);
+    }
+  );
 
 module.exports = router;
---
+++ services/teamService.ts
@@ -0,0 +1,91 @@
+import Team from "../db/models/team";
+import { CreateTeamRequest } from "../data/request/createTeamRequest";
+import Franchise from "../db/models/franchise";
+import { UpdateTeamRequest } from "../data/request/updateTeamRequest";
+
+export const getTeams = async (): Promise<Team[]> => {
+  const teams = await Team.query().where({});
+  return teams;
+};
+
+export const getTeam = async (id: number): Promise<Team> => {
+  return Team.query().findById(id);
+};
+
+export const createTeam = async (team: CreateTeamRequest): Promise<Team> => {
+  const trx = await Franchise.startTransaction();
+  try {
+    let franchise: Franchise | null = null;
+    let firstYear: number = team.firstYear
+      ? team.firstYear
+      : new Date().getFullYear();
+    if (team.franchiseId) {
+      franchise = await Franchise.query()
+        .where({
+          id: team.franchiseId,
+        })
+        .first();
+
+      firstYear = (
+        await Team.query()
+          .where({
+            franchise_id: franchise.id,
+          })
+          .min("first_year")
+          .first()
+      ).first_year;
+    }
+
+    if (!franchise) {
+      franchise = await Franchise.query().insertAndFetch({
+        team_name: team.name,
+      });
+    }
+    trx.commit();
+
+    return Team.query().insertAndFetch({
+      team_name: team.name,
+      name: team.name,
+      active: true,
+      first_year: firstYear,
+      franchise_id: franchise ? franchise.id : 0,
+      location_name: team.location,
+      abbreviation: team.abbreviation,
+      short_name: team.shortName,
+    });
+  } catch (e) {
+    trx.rollback();
+    throw new Error(e);
+  }
+};
+
+export const updateTeam = async (
+  id: number,
+  updates: UpdateTeamRequest
+): Promise<Team> => {
+  const existing = await Team.query().findById(id);
+
+  if (!existing) {
+    throw new Error("Team not found");
+  }
+
+  console.log(existing);
+  const name = updates.name ? updates.name : existing.name;
+  const location_name = updates.location
+    ? updates.location
+    : existing.location_name;
+  const updated = {
+    ...existing,
+    name,
+    short_name: updates.shortName ? updates.shortName : existing.short_name,
+    location_name,
+    team_name: `${location_name} ${name}`,
+    active: updates.active !== undefined ? updates.active : existing.active,
+    abbreviation: updates.abbreviation
+      ? updates.abbreviation
+      : existing.abbreviation,
+  };
+  console.log(updated);
+
+  return Team.query().updateAndFetchById(id, updated);
+};
